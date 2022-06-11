72d028aa1babf84ed75b973577490bc320be4327
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-06-11 13:20:14 -0500

    added Map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 import { Button, Grid, Message, Header, Ref } from "semantic-ui-react";
+
 import Jumbotron from "./components/jumbotron/jumbotron.component";
+import Map from "./components/map/map.component";
 
 const App = () => {
   return (
     <div>
-      <Grid container style={{ padding: "2rem" }}>
+      <Grid container style={{ padding: "2rem 0" }}>
         <Grid.Row>
           <Grid.Column>
             <Jumbotron />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <Map />
           </Grid.Column>
         </Grid.Row>
       </Grid>
--- src/components/jumbotron/jumbotron.component.jsx
+++ src/components/jumbotron/jumbotron.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { Button, Grid, Message, Header, Ref } from "semantic-ui-react";
+import { Button, Message, Header } from "semantic-ui-react";
 
 const Jumbotron = () => {
-  const ExampleButton = React.forwardRef((props, ref) => (
-    <div>
-      <button {...props} ref={ref} />
-    </div>
-  ));
-  const forwardedRef = React.useRef(null);
+  //   const ExampleButton = React.forwardRef((props, ref) => (
+  //     <div>
+  //       <Button {...props} ref={ref} />
+  //     </div>
+  //   ));
+  //   const forwardedRef = React.useRef(null);
 
   return (
     <>
@@ -18,18 +18,16 @@ const Jumbotron = () => {
           This dashboard allows users to view the number of C-19 cases reported
           in each country. Information is derived from the Open Disease Data API
         </p>
-        <Ref innerRef={forwardedRef}>
-          <ExampleButton
-            as="a"
-            href="https://disease.sh/"
-            target="_blank"
-            rel="noreferrer noopener"
-            color="blue"
-            style={{ fontSize: "1.6rem" }}
-          >
-            View API &raquo;
-          </ExampleButton>
-        </Ref>
+        <Button
+          as="a"
+          href="https://disease.sh/"
+          target="_blank"
+          rel="noreferrer noopener"
+          color="blue"
+          style={{ fontSize: "1.6rem" }}
+        >
+          View API &raquo;
+        </Button>
       </Message>
     </>
   );
