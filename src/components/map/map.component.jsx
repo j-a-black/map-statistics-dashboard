c26be05280b5c566d602b49d6e58301713d15ff8
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-06-11 17:43:00 -0500

    fetched data for all countries

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { MapContainer, TileLayer, useMap, Popup, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+
+import { instance } from "../../data/data";
 
 const Map = () => {
   const position = [51.505, -0.09];
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await instance.get("/countries");
+      setData(response.data);
+    }
+
+    getData();
+  }, []);
+
+  console.log(data);
+
+  //   const renderMarkers = data.map((country, index) => (
+  //     <Marker key={index} position={position}>
+  //       <Popup>Your Location</Popup>
+  //     </Marker>
+  //   ));
 
   return (
-    <MapContainer id="map" center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={position}
+      zoom={13}
+      touchZoom={true}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 };
