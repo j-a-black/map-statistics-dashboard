c6b638c20d718b5cdf9530c85a7a3f4188be1159
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-06-12 14:57:52 -0500

    moved api call and useState values from Map Component up to App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import { Button, Grid, Message, Header, Ref } from "semantic-ui-react";
 
@@ -6,6 +7,31 @@ import Jumbotron from "./components/jumbotron/jumbotron.component";
 import Map from "./components/map/map.component";
 
 const App = () => {
+  const [countriesData, setCountriesData] = useState([]);
+  const [globalData, setGlobalData] = useState([]);
+
+  const countries = axios.get("https://disease.sh/v3/covid-19/countries");
+  const all = axios.get("https://disease.sh/v3/covid-19/all");
+
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .all([countries, all])
+        .then(
+          axios.spread((...responses) => {
+            setCountriesData(responses[0].data);
+            setGlobalData(responses[1].data);
+          })
+        )
+        .catch((err) => console.error(err));
+    };
+
+    getData();
+  }, []);
+
+  console.log(countriesData);
+  console.log(globalData);
+
   return (
     <div>
       <Grid container style={{ padding: "2rem 0" }}>
@@ -17,8 +43,12 @@ const App = () => {
 
         <Grid.Row>
           <Grid.Column>
-            <Map />
+            <Map countriesData={countriesData} globalData={globalData} />
           </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>Hi</Grid.Column>
         </Grid.Row>
       </Grid>
     </div>
--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -3,34 +3,33 @@ import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 
 import axios from "axios";
-// import { instance } from "../../data/data";
 
-const Map = () => {
+const Map = ({ countriesData, globalData }) => {
   const position = [51.505, -0.09];
-  const [countriesData, setCountriesData] = useState([]);
-  const [globalData, setGlobalData] = useState([]);
+  // const [countriesData, setCountriesData] = useState([]);
+  // const [globalData, setGlobalData] = useState([]);
 
-  const countries = axios.get("https://disease.sh/v3/covid-19/countries");
-  const all = axios.get("https://disease.sh/v3/covid-19/all");
+  // const countries = axios.get("https://disease.sh/v3/covid-19/countries");
+  // const all = axios.get("https://disease.sh/v3/covid-19/all");
 
-  useEffect(() => {
-    const getData = async () => {
-      await axios
-        .all([countries, all])
-        .then(
-          axios.spread((...responses) => {
-            setCountriesData(responses[0].data);
-            setGlobalData(responses[1].data);
-          })
-        )
-        .catch((err) => console.error(err));
-    };
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     await axios
+  //       .all([countries, all])
+  //       .then(
+  //         axios.spread((...responses) => {
+  //           setCountriesData(responses[0].data);
+  //           setGlobalData(responses[1].data);
+  //         })
+  //       )
+  //       .catch((err) => console.error(err));
+  //   };
 
-    getData();
-  }, []);
+  //   getData();
+  // }, []);
 
-  console.log(countriesData);
-  console.log(globalData);
+  // console.log(countriesData);
+  // console.log(globalData);
 
   const renderAllCountriesNumOfCases = countriesData.map(
     ({ country, countryInfo, cases }) => {
