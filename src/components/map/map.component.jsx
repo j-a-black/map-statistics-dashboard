83e480b420ad977bec2bd7eb07777919ef352130
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-06-11 19:37:45 -0500

    rendered all country location markers in map

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -6,37 +6,57 @@ import { instance } from "../../data/data";
 
 const Map = () => {
   const position = [51.505, -0.09];
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function getData() {
-      const response = await instance.get("/countries");
+    const getData = async () => {
+      let response;
+      try {
+        response = await instance.get("/countries");
+      } catch (e) {
+        console.log("Unable to retreive data");
+        return;
+      }
       setData(response.data);
-    }
+    };
 
     getData();
   }, []);
 
-  console.log(data);
+  const renderData = data.map(({ country, countryInfo, cases }) => {
+    const lat = countryInfo.lat;
+    const long = countryInfo.long;
 
-  //   const renderMarkers = data.map((country, index) => (
-  //     <Marker key={index} position={position}>
-  //       <Popup>Your Location</Popup>
-  //     </Marker>
-  //   ));
+    return (
+      <Marker key={country} position={[lat, long]}>
+        <Popup>{country}</Popup>
+      </Marker>
+    );
+  });
+
+  console.log(renderData);
+
+  // const renderMarkers = data.map((country, index) => (
+  //   <Marker key={index} position={position}>
+  //     <Popup>Your Location</Popup>
+  //   </Marker>
+  // ));
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      touchZoom={true}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <>
+      <MapContainer
+        center={position}
+        zoom={2}
+        touchZoom={true}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {renderData}
+      </MapContainer>
+    </>
   );
 };
 
